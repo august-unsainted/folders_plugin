10297fab7d525d098f780d93ccc0fa23ff2f75cd
august-unsainted <mhidt_unsainted@mail.ru>
2025-12-27 16:49:46 +0800

    ➕ Улучшенный внешний вид

--- folders_plugin.py
+++ folders_plugin.py
@@ -6,6 +6,12 @@ from ui.alert import AlertDialogBuilder
 from client_utils import get_messages_controller
 from base_plugin import BasePlugin, MenuItemData, MenuItemType
 
+from android.widget import EditText, FrameLayout, CheckBox, LinearLayout, TextView, Toast, \
+    ScrollView, CompoundButton
+from android.util import TypedValue
+from android.view import View, ViewGroup
+from android.content.res import ColorStateList
+from org.telegram.ui.ActionBar import Theme
 
 __id__ = "folders"
 __name__ = "Folders"
@@ -14,7 +20,6 @@ __author__ = "@mhidt"
 __version__ = "1.0.0"
 __icon__ = "exteraPlugins/1"
 __min_version__ = "11.12.0"
-DialogObject = find_class("org.telegram.messenger.DialogObject")
 
 
 def log(text: str):
@@ -29,9 +34,28 @@ def reverse_value(text: str) -> str:
     return text.replace("✔️ ", "") if is_active(text) else "✔️ " + text
 
 
+VerticalScrollView = find_class("android.widget.VerticalScrollView")
+
+
+def get_icon_id(icon_name):
+    try:
+        r_drawable = find_class("org.telegram.messenger.R$drawable")
+        field = r_drawable.getField(icon_name)
+        return field.get(None)
+    except Exception as e:
+        print(f"Icon not found: {e}")
+        return 0
+
+margin_dp = 20
+checkbox_tint_list = ColorStateList(
+    [[-16842912], [16842912]],
+    [Theme.getColor(Theme.key_dialogTextHint), Theme.getColor(Theme.key_checkboxCheck)])
+
+
 class FoldersPlugin(BasePlugin):
     def __init__(self):
         super().__init__()
+        self.folders_checkboxes = None
         self.archive = None
         self.controller = None
         self.folders_names = None
@@ -45,7 +69,7 @@ class FoldersPlugin(BasePlugin):
                 menu_type=MenuItemType.PROFILE_ACTION_MENU,
                 text="Редактировать папки",
                 on_click=self.handle_profile_click,
-                icon="user_search"  # Example icon
+                icon="msg_folders"
             )
         )
         self.add_menu_item(
@@ -53,7 +77,7 @@ class FoldersPlugin(BasePlugin):
                 menu_type=MenuItemType.CHAT_ACTION_MENU,
                 text="Редактировать папки",
                 on_click=self.handle_profile_click,
-                icon="user_search"  # Example icon
+                icon="msg_folders"
             )
         )
 
@@ -64,7 +88,6 @@ class FoldersPlugin(BasePlugin):
         fragment = context.get("fragment")
         chat = context.get("chatId")
         chat_id = context.get("dialog_id") or (-chat if chat else None) or context.get("userId")
-        log(f"Message menu item clicked! Chat ID : {chat_id}")
         activity = fragment.getParentActivity()
         if not activity:
             log("Cannot show dialog, no parent activity.")
@@ -82,47 +105,60 @@ class FoldersPlugin(BasePlugin):
         self.folders = raw_folders
         self.folders_ids = ids
         self.folders_names = names
-        self.load_folders(chat_id)
         self.build_dialog(activity, chat_id)
 
     def build_dialog(self, activity, chat_id):
-        def on_item_click(bld: AlertDialogBuilder, index: int):
-            bld.get_dialog()
-            self.folders_names[index] = reverse_value(self.folders_names[index])
-            self.build_dialog(activity, chat_id)
-
         builder = AlertDialogBuilder(activity)
         builder.set_title("Выберите папки")
-        builder.set_items(self.folders_names, on_item_click)
         builder.set_negative_button("Отмена", lambda b, w: b.dismiss())
         save_func = lambda dialog, _, chat=chat_id: self.save_folders(dialog, chat)
         builder.set_positive_button("Сохранить", save_func)
+        try:
+            builder.set_view(self.generate_view(activity, chat_id))
+        except Exception as e:
+            log(str(e))
         builder.show()
 
+    def generate_view(self, activity, chat_id):
+        view = ScrollView(activity)
+        layout = LinearLayout(activity)
+        margin_px = int(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, margin_dp,
+                                                  activity.getResources().getDisplayMetrics()))
+        layout.setOrientation(LinearLayout.VERTICAL)
+        layout.setPadding(margin_px, margin_px // 2, margin_px, margin_px // 4)
+
+        self.folders_checkboxes = []
+        for i, name in enumerate(self.folders_names):
+            checkbox = CheckBox(activity)
+            checkbox.setText(name)
+            checkbox.setTextColor(Theme.getColor(Theme.key_dialogTextBlack))
+            checkbox.setButtonTintList(checkbox_tint_list)
+            folder = self.folders.get(i)
+            if self.is_contains(folder, chat_id)[-1]:
+                checkbox.setChecked(True)
+            layout.addView(checkbox)
+            self.folders_checkboxes.append(checkbox)
+
+        view.addView(layout)
+        return view
+
     def save_folders(self, dialog: AlertDialogBuilder, chat_id: int):
-        for index, folder_id in enumerate(self.folders_ids):
+        for index, checkbox in enumerate(self.folders_checkboxes):
             folder = self.folders.get(index)
-            checked = is_active(self.folders_names[index])
-            always_show = self.get_dialogs(folder)
-            is_contains = always_show.contains(chat_id)
+            dialogs, is_contains = self.is_contains(folder, chat_id)
+            checked = checkbox.isChecked()
             if checked and not is_contains:
                 self.controller.addDialogToFolder(chat_id, 1, -1, 0)
-                always_show.add(chat_id)
+                dialogs.add(chat_id)
             elif not checked and is_contains:
-                always_show.remove(always_show.indexOf(chat_id))
-            set_private_field(folder, "alwaysShow", always_show)
+                dialogs.remove(dialogs.indexOf(chat_id))
+            set_private_field(folder, "alwaysShow", dialogs)
             self.controller.updateFilterDialogs(folder)
 
         log("Folders saved!")
         dialog.dismiss()
 
-    def load_folders(self, chat_id: int) -> None:
-        for i, folder_name in enumerate(self.folders_names):
-            folder = self.folders.get(i)
-            dialogs = self.get_dialogs(folder)
-            if dialogs.contains(chat_id):
-                self.folders_names[i] = reverse_value(folder_name)
-
     @staticmethod
-    def get_dialogs(folder):
-        return get_private_field(folder, "alwaysShow")
+    def is_contains(folder, chat_id):
+        dialogs = get_private_field(folder, "alwaysShow")
+        return dialogs, dialogs.contains(chat_id)
